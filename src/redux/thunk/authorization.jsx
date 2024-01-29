f3181e200cd3773548cb689d6de29947248f837a
ruslanpetrovup <ruslanpetrov163@gmail.com>
2024-01-29 23:35:30 +0200

    fix modal

--- src/redux/thunk/authorization.jsx
+++ src/redux/thunk/authorization.jsx
@@ -1,18 +1,17 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios";
+// import axios from "axios";
 
 export const authorization = createAsyncThunk("Get Users", async () => {
-  const requestOptions = {
-    headers: {
-      "Content-Type": "application/json",
-      token: `${s.token}`,
-    },
-    params: {
-      page: 1,
-      limit: 0,
-    },
-  };
-
+  // const requestOptions = {
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //     token: `${s.token}`,
+  //   },
+  //   params: {
+  //     page: 1,
+  //     limit: 0,
+  //   },
+  // };
   // const res = await axios();
   // console.log("getUsers res = ", res);
 });
--- src/redux/thunk/get-counter.jsx
+++ src/redux/thunk/get-counter.jsx
@@ -2,13 +2,12 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 // import axios from "axios";
 
 export const getCounter = createAsyncThunk("Get Counter", async () => {
-  const requestOptions = {
-    headers: {
-      "Content-Type": "application/json",
-      token: `${s.token}`,
-    },
-  };
-
+  // const requestOptions = {
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //     token: `${s.token}`,
+  //   },
+  // };
   // const res = await axios();
   // console.log("getUsers res = ", res);
 });
