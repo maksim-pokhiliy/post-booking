c7e2e68f9e39c3f453f6d3ddc23170ec26f67c99
ruslanpetrovup <ruslanpetrov163@gmail.com>
2024-01-30 16:05:32 +0200

    remove classNames

--- src/components/shared/NotificationCounter/index.jsx
+++ src/components/shared/NotificationCounter/index.jsx
@@ -1,10 +1,9 @@
 import styles from "./index.module.scss";
 import notification from "../../../assets/icons/notification.svg";
-import classNames from "classnames";
 
 const NotificationCounter = ({ count = 0, onClick = () => {} }) => {
   return (
-    <button className={classNames(styles.notification)} onClick={onClick}>
+    <button className={styles.notification} onClick={onClick}>
       <img className={styles.notificationIcon} src={notification} />
       {count ? (
         <div className={styles.notificationCounter}>{count}</div>
